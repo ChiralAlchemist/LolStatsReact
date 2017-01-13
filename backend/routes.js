a551f590528ce031c73d3088b4f03f1156bb73c6
Joe Ashby <jashby473@gmail.com>
2017-01-13 15:35:51 -0800

    can get the name for everyone in one game

--- backend/routes.js
+++ backend/routes.js
@@ -10,16 +10,13 @@ const routes = function(app) {
     })
     .exec()
     .then(function (user) {
-      console.log(user)
       if(user.length) {
         return res.json({
           user: user
         })
       } else {
-        console.log('hello')
         axios.get("https://na.api.pvp.net/api/lol/na/v1.4/summoner/"+ summonerId+ "?api_key=ff62241d-f02d-443b-8309-c4b10a4bc446")
           .then(function (response) {
-            console.log('response', response)
             new app.mongoose.user({
               id: summonerId,
               name: response.data[summonerId].name,
@@ -44,6 +41,81 @@ const routes = function(app) {
     })
       //res.send(summonerId)
   })
-}
+  app.get('/api/gameInfo/:summonerName', function (req, res) {
+    var summonerName = req.params.summonerName
+    axios.get(`https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/${summonerName}?api_key=ff62241d-f02d-443b-8309-c4b10a4bc446`)
+    .then(function (response) {
+      var summonerId = response.data[summonerName.toLowerCase()].id
+      return axios.get(`https://na.api.pvp.net/api/lol/na/v1.3/game/by-summoner/${summonerId}/recent?api_key=ff62241d-f02d-443b-8309-c4b10a4bc446`)
+    })
+    .then(function (response) {
+      var games = response.data.games
+      console.log('made it here', games )
+      var count = 0;
+      return Promise.all(games.slice(0,1).map(function (game) {
+        return Promise.all(game.fellowPlayers.map(function (player){
+          var summonerId = player.summonerId
+          count++;
+          console.log(count);
+          return getSummonerName(summonerId)//Promise.resolve(axios.get("https://na.api.pvp.net/api/lol/na/v1.4/summoner/"+ summonerId+ "?api_key=ff62241d-f02d-443b-8309-c4b10a4bc446"))//`https://na.api.pvp.net/api/lol/na/v1.4/summoner/${summonerId}/recent?api_key=ff62241d-f02d-443b-8309-c4b10a4bc446`))
+          .catch(function (err){
+            var retryTime = err.headers['retry-after'] * 1000;
+            var summonerId = err.config.url.match(/summoner\/\d+/g)[0].slice(9)
+            console.log("at error summonerId")
+            return new Promise(function (resolve, reject) {
+              setTimeout(function (){
+                resolve(getSummonerName(summonerId))
+              }, retryTime)
+            })
+            // return res.json({
+            //   failed : true,
+            //   retryTime: retryTime,
+            //   err : err
+            // })
+          })
+        }
+      ))
+        // console.log('2')
+        // console.log(game.fellowPlayers)
+        // return Promise.all(
+        //   // game.fellowPlayers.map(function (player) {
+        //   // var summonerId = player.summonerId
+        //   // console.log('3', summonerId)
+        //   // console.log('player', player)
+        //   // return axios.get(`https://na.api.pvp.net/api/lol/na/v1.4/summoner/${summonerId}/recent?api_key=ff62241d-f02d-443b-8309-c4b10a4bc446`)
+        //   // .then(function (result) {
+        //   //   console.log('this is the player info', result)
+        //   //   return result
+        //   // })
+        // }))
+      }))
+      .then(function (result) {
+        console.log('made it to this spot')
+        return res.json({
+          succuss: true,
+          result: result
+        })
+      })
 
+    })
+  })
+
+}
+function getSummonerName (summonerId) {
+  console.log('hello from get summoner name')
+  return axios.get("https://na.api.pvp.net/api/lol/na/v1.4/summoner/"+ summonerId+ "?api_key=ff62241d-f02d-443b-8309-c4b10a4bc446")
+  .then(function (res){
+    return Promise.resolve([res.data[summonerId]])
+  })
+  // .catch(function (err){
+  //   var retryTime = err.headers['retry-after'] * 1000;
+  //   var summonerId = err.config.url.match(/summoner\/\d+/g)[0].slice(9)
+  //   console.log("rateLimit error for summonerId", summonerId)
+  //   return new Promise(function (resolve, reject) {
+  //     setTimeout(function (){
+  //       resolve(getSummonerName(summonerId))
+  //     }, retryTime)
+  //   })
+  // })
+}
 module.exports = routes;
